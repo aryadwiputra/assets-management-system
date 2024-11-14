cf82410cb59a92de110a3ff401af4f75909d540d
Arya Dwi Putra <aryadptr.developer@gmail.com>
2024-11-14 09:11:26 +0700

    refactor: Added code to handle Excel file import in AssetController, including loading spreadsheet, reading worksheet data, and extracting images from the file.

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -296,5 +296,38 @@ class AssetController extends Controller
         } catch (\Exception $e) {
             return redirect()->back()->with('error', 'Terjadi kesalahan saat mengimpor data aset.');
         }
+        // $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($request->file('file'));
+
+        // $worksheet = $spreadsheet->getActiveSheet();
+        // $worksheetArray = $worksheet->toArray();
+        // array_shift($worksheetArray);
+
+        // echo '<table style="width:100%"  border="1">';
+        // echo '<tr align="center">';
+        // echo '<td>Sno</td>';
+        // echo '<td>Name</td>';
+        // echo '<td>Image</td>';
+        // echo '</tr>';
+
+        // foreach ($worksheetArray as $key => $value) {
+
+        //     $worksheet = $spreadsheet->getActiveSheet();
+        //     $drawing = $worksheet->getDrawingCollection()[$key];
+
+        //     $zipReader = fopen($drawing->getPath(), 'r');
+        //     $imageContents = '';
+        //     while (!feof($zipReader)) {
+        //         $imageContents .= fread($zipReader, 1024);
+        //     }
+        //     fclose($zipReader);
+        //     $extension = $drawing->getExtension();
+
+        //     echo '<tr align="center">';
+        //     echo '<td>' . $value[0] . '</td>';
+        //     echo '<td>' . $value[1] . '</td>';
+        //     echo '<td><img  height="150px" width="150px"   src="data:image/jpeg;base64,' . base64_encode($imageContents) . '"/></td>';
+        //     echo '</tr>';
+
+        // }
     }
 }
