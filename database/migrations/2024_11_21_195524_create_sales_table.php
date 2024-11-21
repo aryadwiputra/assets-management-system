4b04b8b4a6b60be2f6b14868f7175d991e271d2d
Arya Dwi Putra <aryadptr.developer@gmail.com>
2024-11-21 21:26:35 +0700

    feat: Create asset sales transactions migrations, model and controller

---
+++ database/migrations/2024_11_21_195524_create_sales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('pic_id');
+            $table->foreign('pic_id')->references('id')->on('person_in_charges')->onDelete('cascade')->onUpdate('cascade');
+            $table->date('date');
+            $table->integer('price');
+            $table->string('buyer_name');
+            $table->string('buyer_contact');
+            $table->longText('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
